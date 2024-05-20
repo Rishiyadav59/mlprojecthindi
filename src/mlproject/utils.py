c418b01dd620afaa6c2a913c22b477cf50a25374
Rishiyadav59 <150953062+Rishiyadav59@users.noreply.github.com>
2024-05-20 06:30:58 +0530

    model training

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,7 +4,10 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pymysql
+
 import pickle
 import numpy as np
 
@@ -13,8 +16,7 @@ load_dotenv()
 host=os.getenv("host")
 user=os.getenv("user")
 password=os.getenv("password")
-db=os.getenv("db")
-
+db=os.getenv('db')
 
 
 
@@ -26,7 +28,6 @@ def read_sql_data():
             user=user,
             password=password,
             db=db
-            
         )
         logging.info("Connection Established",mydb)
         df=pd.read_sql_query('Select * from students',mydb)
@@ -34,8 +35,10 @@ def read_sql_data():
 
         return df
 
-    except Exception as e:
-        raise CustomException(e,sys)
+
+
+    except Exception as ex:
+        raise CustomException(ex)
     
 def save_object(file_path, obj):
     try:
@@ -45,6 +48,37 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
